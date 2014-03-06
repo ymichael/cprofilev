506a6592a0eeddd9a21a03097af936af17f438de
Michael Yong <wrong92@gmail.com>
2014-03-07 01:45:24 +0800

    WIP Refactor
    - Use argparse
    - Use classes for bottle app instead of global variables and functions

--- cprofilev.py
+++ cprofilev.py
@@ -1,30 +1,16 @@
 #!/usr/bin/env python
 
-from bottle import route, run, template, request
+from bottle import Bottle, template, request
 from pstats import Stats
 from StringIO import StringIO
-import sys
+import argparse
 import re
 
-
-def read_value(self):
-    value = self.stream.getvalue()
-    self.stream = StringIO()
-    return value
-
-
-Stats.read_value = read_value
-
-
-global_obj = {
-    'cprofile_output': None,
-    'stats_obj': None,
-}
 
 stats_template = """\
     <html>
         <head>
-            <title>{{ filename }} | cProfile Results</title>
+            <title>cProfile Results</title>
         </head>
         <body>
             <pre>{{ !stats }}</pre>
@@ -34,14 +20,6 @@ stats_template = """\
             <pre>{{ !callees }}</pre>
         </body>
     </html>"""
-
-
-def get_flag_value(flag):
-    try:
-        return sys.argv[sys.argv.index(flag) + 1]
-    except:
-        pass
-    return None
 
 SORT_ARGS = ['ncalls', 'cumtime', 'tottime', 'filename']
 
@@ -71,48 +49,99 @@ def process_stats(stats):
         result.append(line)
     return '\n'.join(result)
 
+# @route('/')
+# def index():
+#     sort = request.query.sort or 'cumulative'
+#     global_obj['stats_obj'].sort_stats(sort)
+#     global_obj['stats_obj'].print_stats(request.query.func)
+#     stats = global_obj['stats_obj'].read_value()
+#
+#     if request.query.func:
+#         func = request.query.func
+#         global_obj['stats_obj'].print_callers(func)
+#         callers = global_obj['stats_obj'].read_value()
+#
+#         global_obj['stats_obj'].print_callees(func)
+#         callees = global_obj['stats_obj'].read_value()
+#     else:
+#         callers = ''
+#         callees = ''
+#
+#     data = {
+#         'filename': global_obj['cprofile_output'],
+#         'stats': process_stats(stats),
+#         'callers': process_stats(callers),
+#         'callees': process_stats(callees),
+#     }
+#     return template(stats_template, **data)
 
-@route('/')
-def index():
-    sort = request.query.sort or 'cumulative'
-    global_obj['stats_obj'].sort_stats(sort)
-    global_obj['stats_obj'].print_stats(request.query.func)
-    stats = global_obj['stats_obj'].read_value()
 
-    if request.query.func:
-        func = request.query.func
-        global_obj['stats_obj'].print_callers(func)
-        callers = global_obj['stats_obj'].read_value()
+class CProfileVStats(object):
+    """Wrapper around pstats.Stats class."""
+    def __init__(self, output_file):
+        self.output_file = output_file
+        self.obj = Stats(output_file)
+        self.reset_stream()
 
-        global_obj['stats_obj'].print_callees(func)
-        callees = global_obj['stats_obj'].read_value()
-    else:
+    def reset_stream(self):
+        self.obj.stream = StringIO()
+
+    def read(self):
+        value = self.obj.stream.getvalue()
+        self.reset_stream()
+        return value
+
+    def show(self, restriction=''):
+        self.obj.print_stats(restriction)
+        return self
+
+
+
+class CProfileV(object):
+    def __init__(self, cprofile_output, port=4000, quiet=True):
+        self.cprofile_output = cprofile_output
+        self.port = port
+        self.quiet = quiet
+        self.app = Bottle()
+        self.stats_obj = CProfileVStats(self.cprofile_output)
+
+        # init route.
+        self.app.route('/')(self.route_handler)
+
+    def route_handler(self):
+        stats = self.stats_obj.show().read()
         callers = ''
         callees = ''
-
-    data = {
-        'filename': global_obj['cprofile_output'],
-        'stats': process_stats(stats),
-        'callers': process_stats(callers),
-        'callees': process_stats(callees),
-    }
-    return template(stats_template, **data)
+        data = {
+            'stats': process_stats(stats),
+            'callers': process_stats(callers),
+            'callees': process_stats(callees),
+        }
+        return template(stats_template, **data)
 
 
-def usage():
-    pass
+    def start(self):
+        """Starts bottle server."""
+        self.app.run(host='localhost', port=self.port, quiet=self.quiet)
+
 
 def main():
-    # TODO(michael): Make more robust
-    flag = get_flag_value('-p')
-    if flag:
-        global_obj['cprofile_output'] = flag
-        stats_obj = Stats(global_obj['cprofile_output'])
-        stats_obj.stream = StringIO()
-        global_obj['stats_obj'] = stats_obj
-        run(host='localhost', port=8080)
-    else:
-        usage()
+    parser = argparse.ArgumentParser(
+        description='Thin wrapper for viewing python cProfile output.')
+    # TODO(michael): Read version from some config file.
+    parser.add_argument('--version', action='version', version='0.0.1')
+    parser.add_argument('-v', '--verbose', action='store_const', const=True)
+    parser.add_argument('-p', '--port', type=int, default=4000,
+        help='specify the port to listen on. (defaults to 4000)')
+    parser.add_argument('cprofile_output', help='The cProfile output to view.')
+    args = vars(parser.parse_args())
+
+    port = args['port']
+    cprofile_output = args['cprofile_output']
+    quiet = not args['verbose']
+
+    CProfileV(cprofile_output, port, quiet).start()
+
 
 if __name__ == '__main__':
     main()
